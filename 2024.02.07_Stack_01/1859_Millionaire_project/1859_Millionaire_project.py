d7bb3086140d408b91f7678511b38265b2a5cce9
Taeram Ryu <ryu.taeram@gmail.com>
2024-02-07 20:18:46 +0900

    Stack 01

--- 2024.02.07_Stack_01/1859_Millionaire_project/1859_Millionaire_project.py
+++ 2024.02.07_Stack_01/1859_Millionaire_project/1859_Millionaire_project.py
@@ -7,26 +7,31 @@ for t in range(1, T + 1):
     N = int(input())
     arr = list(map(int, input().split()))
 
-    arr_re = arr[::-1] + [0]
-    total_cost = 0
-    cnt = 1
+    arr_re = arr[::-1]
     profit = 0
-
-    storage = 0
+    max_cost = arr_re[0]
     for n in range(N):
-        if arr_re[n] > arr_re[n + 1]:
-            storage = arr_re[n]
-        elif arr_re[n] < arr_re[n + 1]:
-            if arr_re[n] > storage:
-                profit += storage * cnt
-                cnt = 1
-                storage = arr_re[n]
-                continue
-            elif arr_re[n] < arr_re[n + 1]:
-                cnt += 1
-        elif arr_re[n] == arr_re[n + 1]:
-            continue
-    print(profit)
+        if arr_re[n] > max_cost:
+            max_cost = arr_re[n]
+        else:
+            profit += max_cost - arr_re[n]
+    print(f'#{t} {profit}')
+    # for n in range(N):
+    #     if arr_re[n] > arr_re[n + 1]:
+    #         if max_cost > arr_re[n]:
+    #             storage.append(arr_re[n])
+    #         elif max_cost < arr_re[n]:
+    #             max_cost = arr_re[n]
+    #     elif arr_re[n] <= arr_re[n + 1]:
+    #         if max_cost > arr_re[n]:
+    #             profit += max_cost * len(storage)
+    #             max_cost = arr_re[n]
+    #             continue
+    #         elif arr_re[n] < arr_re[n + 1]:
+    #             storage.append(arr_re[n])
+    #     elif arr_re[n] == arr_re[n + 1]:
+    #         continue
+    # print(profit)
     # if arr[0] <= arr[1]:
     #     cnt += 1
     #     total_cost += arr[0]
